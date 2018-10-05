a7113c06472b2f8a2ac1f7fa191c866cc9ff25c8
= <rlokesh@aconex.com>
2018-10-05 09:57:13 +0530

    storing previous calculations of fibanocci to improve efficency

--- Assignment2/src/main/java/Fibanocci.java
+++ Assignment2/src/main/java/Fibanocci.java
@@ -1,11 +1,16 @@
 public class Fibanocci {
     public static int fibanocciNumber(int number) {
+        int[] FibanocciNumberStorage = new int[number+1];
         if (number==1 || number==2) {
             return 1;
+        }
+        else if (FibanocciNumberStorage[number] != 0){
+            return FibanocciNumberStorage[number];
         }
         else {
             int fibanocciNum;
             fibanocciNum=fibanocciNumber(number-1)+fibanocciNumber(number-2);
+            FibanocciNumberStorage[number]=fibanocciNum;
             return fibanocciNum;
         }
     }
