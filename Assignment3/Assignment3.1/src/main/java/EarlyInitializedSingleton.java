0e67edd641582407b29293f47fa67d2b90ed5260
= <rlokesh@aconex.com>
2018-10-27 22:40:41 +0530

    singleton using enum

--- Assignment3/Assignment3.1/src/main/java/EarlyInitializedSingleton.java
+++ Assignment3/Assignment3.1/src/main/java/EarlyInitializedSingleton.java
@@ -1,12 +1,11 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
-public class Singleton1 {
+public class Singleton implements Serializable {
     private static Singleton1 ourInstance = new Singleton1();
 
     public static Singleton1 getInstance() {
         return ourInstance;
     }
 
-    private Singleton1() {
-    }
+    private Singleton1() { }
 }
