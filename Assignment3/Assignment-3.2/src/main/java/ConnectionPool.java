36ca1632516ae5e4d91ee645c98123144ec6500e
= <rlokesh@aconex.com>
2018-10-30 09:21:12 +0530

    Abstracting DB Configuration

--- Assignment3/Assignment-3.2/src/main/java/ConnectionPool.java
+++ Assignment3/Assignment-3.2/src/main/java/ConnectionPool.java
@@ -1,56 +1,27 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 class ConnectionPool {
-    private static String DB_URL;
-    private static String DB_USER_NAME;
-    private static String DB_DRIVER;
-    private static String DB_USER_PASSWORD;
-    private static int MAX_CONNECTIONS;
-
-    private static void init() {
-        Properties properties = new Properties ( );
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream ( "src/main/resources/configuration.properties" );
-            properties.load ( inputStream );
-        } catch ( FileNotFoundException e ) {
-            e.printStackTrace ( );
-        } catch ( IOException e ) {
-            e.printStackTrace ( );
-        }
-        DB_DRIVER = properties.getProperty ( "DB_DRIVER" );
-        DB_URL = properties.getProperty ( "DB_URL" );
-        DB_USER_NAME = properties.getProperty ( "DB_USER_NAME" );
-        DB_USER_PASSWORD = properties.getProperty ( "DB_USER_PASSWORD" );
-        MAX_CONNECTIONS = Integer.parseInt ( properties.getProperty ( "MAX_CONNECTIONS" ) );
-    }
 
     private static final ConnectionPool INSTANCE = new ConnectionPool ( );
+    private static int totalconnections = 0;
+    final List<Connection> availableConnections = new ArrayList<Connection> ( );
+
 
     private ConnectionPool() {
-        ConnectionPool.init ( );
+
     }
 
     public static ConnectionPool getInstance() {
         return INSTANCE;
     }
 
-
-    final List<Connection> availableConnections = new ArrayList<Connection> ( );
-    private static int totalconnections = 0;
-
     private Connection createNewConnection() throws ClassNotFoundException, SQLException {
-        Class.forName ( DB_DRIVER );
-        return DriverManager.getConnection ( DB_URL, DB_USER_NAME, DB_USER_PASSWORD );
+        Class.forName ( DbConfiguration.getDbDriver ( ) );
+        return DriverManager.getConnection ( DbConfiguration.getDbUrl ( ), DbConfiguration.getDbUserName ( ), DbConfiguration.getDbUserPassword ( ) );
     }
 
     public Connection getConnection() throws Exception {
@@ -58,7 +29,7 @@ class ConnectionPool {
             Connection connection = availableConnections.get ( 0 );
             availableConnections.remove ( 0 );
             return connection;
-        } else if (totalconnections < MAX_CONNECTIONS) {
+        } else if (totalconnections < DbConfiguration.getMaxConnections ( )) {
             Connection newConnection = createNewConnection ( );
             totalconnections++;
             return newConnection;
